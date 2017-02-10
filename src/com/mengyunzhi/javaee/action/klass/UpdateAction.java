025b41f622bc9a73925713259d28d3dc795b87d7
mskies <3792535@qq.com>
2017-02-10 17:01:51 +0800

    step12.5.2

--- src/com/mengyunzhi/javaee/action/klass/UpdateAction.java
+++ src/com/mengyunzhi/javaee/action/klass/UpdateAction.java
@@ -1,8 +1,11 @@
 package com.mengyunzhi.javaee.action.klass;
 
+import com.mengyunzhi.javaee.action.Action;
 import com.opensymphony.xwork2.validator.annotations.RequiredFieldValidator;
+import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
+import com.opensymphony.xwork2.validator.annotations.StringLengthFieldValidator;
 
-public class UpdateAction extends SaveAction {
+public class UpdateAction extends Action {
 
     private static final long serialVersionUID = 1L;
     private Long id;
@@ -18,7 +21,29 @@ public class UpdateAction extends SaveAction {
             this.addFieldError("id", "传入的班级ID不存在或已删除");
         }
     }
+    private String name;
+    private Long teacherId;
+
+    public String getName() {
+        return name;
+    }
+
+    @RequiredStringValidator(message = "名称不能为空")
+    @StringLengthFieldValidator(minLength = "2", maxLength = "8", trim = true, message = "名称必须介于2-8之间")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTeacherId(Long teacherId) {
+        if(teacherId == 200) {
+            this.addFieldError("teacherId", "传入的teacherId无效");
+        }
+        this.teacherId = teacherId;
+    }
     
+    public Long getTeacherId() {
+        return this.teacherId;
+    }
     public String execute() {
         return SUCCESS;
     }
